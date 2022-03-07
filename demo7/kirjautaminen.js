a1c008986cb0c39f59ff216b4f941fbf276578d6
CasperSeppern <Casper.seppern@edu.keuda.fi>
2022-03-07 09:52:38 +0200

    Vaihdoin koodi että sivusto voisi tallenta nimeen ja sen jälkeen sanoa tervetuloa + henkilön nimi

---
+++ demo7/kirjautaminen.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", onkoKirjautanut);
+
+function onkoKirjautanut(){
+    let kirjautanut = 'kylla';
+    if(kirjautanut === 'kylla'){
+       document.getElementById('tervetulo_teksti').textContent += localStorage.getItem("nimi");
+    }
+}
+
+function kirjaudu(){
+    localStorage.setItem("nimi", docunent.getElementById("nimi").value);
+}
