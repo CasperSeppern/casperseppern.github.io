cd2deede73cea6685d71064b0e96136650a401e1
Casper.Seppern <Casper.seppern@edu.keuda.fi>
2022-03-07 18:44:38 +0200

    Pistin (if else) Statements

--- demo8/kirjautaminen.js
+++ demo8/kirjautaminen.js
@@ -4,14 +4,19 @@ function onkoKirjautanut(){
     if(localStorage.getItem("kirjautunut") === "kylla") {
        document.getElementById("tervetulo_teksti").textContent += localStorage.getItem("nimi");
        document.getElementById("kirjautumis_lomake").style.display = "none";
-       document.getElementById("kirjaudu ulos").style.display="true";
+      
+    }
+    else{
+        document.getElementById("kirjaudu ulos").style.display="none";
     }
     
 }
 
 function kirjaudu(){
+    document.getElementById("kirjaudu ulos").style.display="none";
     localStorage.setItem("nimi", document.getElementById("nimi").value);
     localStorage.setItem("kirjautunut", "kylla")
+    
 
 
 }
