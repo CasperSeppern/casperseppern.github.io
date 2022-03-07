bdb7a2926539dc247c8dda0ce04948ad7b9345d4
CasperSeppern <Casper.seppern@edu.keuda.fi>
2022-03-07 10:22:53 +0200

    Document oli kirjoitettu väärin ja korjaasin ongelman

--- demo7/kirjautaminen.js
+++ demo7/kirjautaminen.js
@@ -2,11 +2,11 @@ document.addEventListener("DOMContentLoaded", onkoKirjautanut);
 
 function onkoKirjautanut(){
     let kirjautanut = 'kylla';
-    if(kirjautanut === 'kylla'){
+    if(kirjautanut === 'kylla') {
        document.getElementById("tervetulo_teksti").textContent += localStorage.getItem("nimi");
     }
 }
 
 function kirjaudu(){
-    localStorage.setItem("nimi", docunent.getElementById("nimi").value);
+    localStorage.setItem("nimi", document.getElementById("nimi").value);
 }
